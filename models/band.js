73f9db460a316f9d977ccbf122efdfeb26f58662
JDiaz <jdiaz_007@outlook.com>
2023-10-19 20:19:33 -0700

    updated note format

--- models/band.js
+++ models/band.js
@@ -4,13 +4,11 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Band extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
+    //  * Helper method for defining associations.
+    //  * This method is not a part of Sequelize lifecycle.
+    //  * The `models/index` file will call this method automatically.
     static associate(models) {
-      // define association here
+      // * define association here
     }
   }
   Band.init({
