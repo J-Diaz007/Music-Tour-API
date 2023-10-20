e8939864778a9bf23b4ad61deb45738fe5494580
JDiaz <jdiaz_007@outlook.com>
2023-10-19 21:08:02 -0700

    finished day 4 activity

---
+++ seeders/20231020032201-bands.js
@@ -0,0 +1,29 @@
+'use strict';
+
+// * @type {import('sequelize-cli').Migration}
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    //  * Example:
+    //  * await queryInterface.bulkInsert('People', [{
+    //  *   name: 'John Doe',
+    //  *   isBetaMember: false
+    //  * }], {});
+    await queryInterface.bulkInsert('bands', [{
+      name: 'Metallica',
+      genre: 'Heavy Metal',
+      available_start_time: '21:00:00',
+      end_time: '23:00:00'
+    }])
+  },
+
+  async down (queryInterface, Sequelize) {
+    //  * Example:
+    //  * await queryInterface.bulkDelete('People', null, {});
+    // * Example below deletes everything inside the table
+    await queryInterface.bulkDelete('bands', null, {})
+
+  }
+};
+
+'use strict'
