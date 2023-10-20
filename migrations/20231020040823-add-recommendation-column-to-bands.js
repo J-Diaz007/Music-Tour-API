156b3079e7993d3e0c75d6bcdb25527f8538ddac
JDiaz <jdiaz_007@outlook.com>
2023-10-19 21:35:23 -0700

    added recommendation column to bands

---
+++ migrations/20231020040823-add-recommendation-column-to-bands.js
@@ -0,0 +1,23 @@
+'use strict';
+const { DataTypes } = require('sequelize')
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.addColumn('bands', 'recommendation', {
+      type: DataTypes.STRING
+    })
+  },
+
+  async down (queryInterface, Sequelize) {
+
+  }
+};
+
+// * Inside the up method, call the addColumn method on queryInterface.
+// * Notice how the up method is defined as async? That means we can await our method call.
+// * The first argument the addColumn method accepts is the name of the table. In this case, "bands"
+// * Second argument the name of the column we want to add. In this case, "recommendation"
+// * Third argument is an options object to specify the column's datatype, and any other options we want, such as a default value or whether to allow null values. 
+   // * In this case, we want the datatype to be a string.
+// * Note that because we're using the DataTypes class, we need to import it into this file. 
+// * At the top, underneath the 'use strict' line, destructure DataTypes from requiring the Sequelize package.
